47f349afd104dcc801ea9b3adb2624aa6cc193b0
idangg <dang.isabella@gmail.com>
2017-04-25 22:29:19 -0700

    final

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+   $("#breadimg").click(function(){
+      $("#modal-container").show();
+   });
+
+   $("#modal-overlay").click(function(){
+      $("#modal-container").hide();
+   }) 
+
+    $("#about").click(function(){
+      $("#modal-container2").show();
+   });
+
+
+   $("#modal-overlay").click(function(){
+      $("#modal-container").hide();
+   }) 
+   
+
+
+   
+});
